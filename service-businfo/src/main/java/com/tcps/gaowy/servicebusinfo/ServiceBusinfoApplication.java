8e81fa323fc70b5dd4fd34322d4f8e8a17b5a1b3
Gaowy <317326646@qq.com>
2018-02-11 18:10:22 +0800

    开始测试，单独编译子模块。

---
+++ service-businfo/src/main/java/com/tcps/gaowy/servicebusinfo/ServiceBusinfoApplication.java
@@ -0,0 +1,12 @@
+package com.tcps.gaowy.servicebusinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBusinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBusinfoApplication.class, args);
+	}
+}
