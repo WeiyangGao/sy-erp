9d9d8e780c7e6611ca884157cb3c0fc2afd1f96f
Gaowy <317326646@qq.com>
2018-02-26 10:36:47 +0800

    读取yml 自定义配置信息，文件上传ok

--- service-android/src/main/java/com/tcps/gaowy/serviceandroid/controller/FileController.java
+++ service-android/src/main/java/com/tcps/gaowy/serviceandroid/controller/FileController.java
@@ -1,7 +1,9 @@
 package com.tcps.gaowy.serviceandroid.controller;
 
 import com.tcps.gaowy.basecore.pojo.FileInfo;
+import com.tcps.gaowy.serviceandroid.ConfigVlaue;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,18 +18,23 @@ import java.util.Date;
 @Slf4j
 @RestController
 @RequestMapping("/file")
-@PropertySource({"classpath:config/application.yml"})
 public class FileController {
-
-    @Value("${file.path}")
-    private String filePath;
+    @Autowired
+    ConfigVlaue configVlaue;
 
     @PostMapping
     public FileInfo upload(MultipartFile file) throws IOException {
+        String filePath = configVlaue.getFileUploadPath();
         log.info(file.getName());
         log.info(file.getOriginalFilename());
         log.info(file.getSize() + "");
         File localFile = new File(filePath, new Date().getTime() + ".txt");
+        if (!localFile.exists()) {
+            boolean result = localFile.getParentFile().mkdirs();
+            if (!result) {
+                log.info("创建失败！");
+            }
+        }
         file.transferTo(localFile);
         return new FileInfo(localFile.getAbsolutePath());
     }
